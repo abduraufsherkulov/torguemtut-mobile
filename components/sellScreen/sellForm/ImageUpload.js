66d4b2979135f29cccb44f853c5f56c114fb73a3
Abdurauf Sherkulov <abduraufsherkulov@gmail.com>
2020-02-20 23:47:23 +0500

    photo gallery

--- components/sellScreen/sellForm/ImageUpload.js
+++ components/sellScreen/sellForm/ImageUpload.js
@@ -53,54 +53,59 @@ function ImageUpload() {
             quality: 1
         });
 
-        // console.log(result);
-
         if (!result.cancelled) {
-            // const endpoint = "https://ttuz.azurewebsites.net/api/news/upload-image";
-            // const data = new FormData();
-            // data.append('image', result.uri);
-
-            // axios({
-            //     method: "post",
-            //     url: endpoint,
-            //     data: data,
-            //     headers: {
-            //         'Accept': 'application/json',
-            //         "Content-Type": "multipart/form-data",
-            //         Authorization: `Bearer ${userData.token}`
-            //     }
-            // }).then(response => {
-            //     console.log(response);
-            // }).catch(err => {
-            //     console.log(err)
-            // })
+            const endpoint = "https://ttuz.azurewebsites.net/api/news/upload-image";
+            const data = new FormData();
+            let name = result.uri.split("/");
+            name = name[name.length - 1];
+            let type = name.split(".");
+            type = type[type.length - 1];
+            type = `image/${type}`;
+            data.append('image', { uri: result.uri, name: name, filename: name, type: type });
+            data.append('Content-Type', type);
+            axios({
+                method: "post",
+                url: endpoint,
+                data: data,
+                headers: {
+                    'Accept': 'application/json',
+                    "Content-Type": "multipart/form-data",
+                    Authorization: `Bearer ${userData.token}`
+                }
+            }).then(response => {
+                console.log(response);
+            }).catch(err => {
+                console.log(err, 'in add photo')
+            })
             image[index].uri = result.uri;
             // console.log(image)
             setImage([...image])
         }
     };
-    console.log(image)
+    console.log(userData.token)
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', marginHorizontal: 8 }}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Button
                 title="Pick an image from camera roll"
                 onPress={_pickImage}
             />
             <View style={{ flex: 1, flexWrap: 'wrap', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
                 {[0, 1, 2, 3, 4, 5, 6, 7].map((index) => (
-                    <View key={index} style={{ width: Dimensions.get('window').width / 4 - 10, backgroundColor: '#eaeaea', alignItems: 'center', marginVertical: 2 }}>
-                        <TouchableOpacity
-                            style={{ alignItems: 'center', paddingVertical: 12 }}
-                            onPress={() => _pickImage(index)}>
-                            {image[index].uri ?
-                                <Image source={{ uri: image[index].uri }} style={{ width: Dimensions.get('window').width / 4, height: 100 }} /> :
-                                <React.Fragment>
-                                    <Ionicons name="ios-images" size={35} color="black" />
-                                    <Text style={{ textAlign: 'center', fontSize: 10 }}>{index == 0 ? 'Добавить фото' : index + 1}</Text>
-                                </React.Fragment>
-                            }
-                        </TouchableOpacity>
-                    </View>
+                    <React.Fragment>
+                        {image[index].uri ?
+                            <Image key={index * Math.random()} source={{ uri: image[index].uri }} style={{ width: Dimensions.get('window').width / 4 - 10, height: 75, marginVertical: 2 }} /> :
+                            <View key={index} style={{ width: Dimensions.get('window').width / 4 - 10, height: 75, backgroundColor: '#eaeaea', alignItems: 'center', marginVertical: 2 }}>
+                                <TouchableOpacity
+                                    style={{ alignItems: 'center', paddingVertical: 12 }}
+                                    onPress={() => _pickImage(index)}>
+                                    <React.Fragment>
+                                        <Ionicons name="ios-images" size={35} color="black" />
+                                        <Text style={{ textAlign: 'center', fontSize: 10 }}>{index == 0 ? 'Добавить фото' : index + 1}</Text>
+                                    </React.Fragment>
+                                </TouchableOpacity>
+                            </View>
+                        }
+                    </React.Fragment>
                 ))}
             </View>
         </View>
